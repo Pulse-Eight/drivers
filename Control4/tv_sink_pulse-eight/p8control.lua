e3725333917bd26ce9a0e618e188e626c3a076ca
Martin Ellis <malard@gmail.com>
2016-07-28 22:56:53 +0100

    fixed: did not commit all files

--- Control4/tv_sink_pulse-eight/p8control.lua
+++ Control4/tv_sink_pulse-eight/p8control.lua
@@ -12,6 +12,50 @@ end
 
 function P8INT:DEVICE_POWER(on)
     local port = P8INT:GET_OUTPUT_PORT()
-    local uri = P8INT:GET_MATRIX_URL() .. "/cec/" .. (on ? "on" : "off") .. "/Output/" .. port
+    local state = "on"
+    if not on then
+	   state = "off"
+    end
+    local uri = P8INT:GET_MATRIX_URL() .. "/cec/" .. state .. "/Output/" .. port
     C4:urlGet(uri)
+end
+
+
+--the code below is still in development and cannot be used yet.
+function P8INT:UPDATE_VOLUME_LEVEL()
+    local port = P8INT:GET_OUTPUT_PORT()
+    
+    local c4Level = ConvertVolumeToC4(50, 0, 100)
+    local tParams = {LEVEL = c4Level, OUTPUT = 4000}
+    
+    SendNotify("VOLUME_LEVEL_CHANGED", tParams, 5001)	
+end
+
+function ConvertVolumeToC4(volume, minDeviceLevel, maxDeviceLevel)
+	--to be used when converting a volume level from a device to a 
+	--percentage value that can be used by C4 proxies
+	--"volume" is the volume value from the device
+	--"minDeviceLevel" & "maxDeviceLevel" are the minimum and maximum volume levels
+	--as specified in the device protocol documentation
+	return ProcessVolumeLevel(volume, minDeviceLevel, maxDeviceLevel, 0, 100)
+end
+
+function ConvertVolumeToDevice(volume, minDeviceLevel, maxDeviceLevel)
+	--to be used when converting a volume level from a C4 proxy to a 
+	--value that can be used by the device 
+	--"volume" is the volume value from the C4 proxy
+	--"minDeviceLevel" & "maxDeviceLevel" are the minimum and maximum volume levels
+	--as specified in the device protocol documentation
+	return ProcessVolumeLevel(volume, 0, 100, minDeviceLevel, maxDeviceLevel)
+end
+
+function ProcessVolumeLevel(volLevel, minVolLevel, maxVolLevel, minDeviceLevel, maxDeviceLevel)
+	  local level = (volLevel-minVolLevel)/(maxVolLevel-minVolLevel)
+	  --LogInfo("level = " .. level)
+	  local vl=(level*(maxDeviceLevel-minDeviceLevel))+minDeviceLevel
+	  --LogInfo("vl = " .. vl)
+	  vl= tonumber(("%.".."0".."f"):format(vl))
+	  --LogInfo("vl new = " .. vl)
+	  LogInfo("ProcessVolumeLevel(level in=" .. volLevel .. ", level out=" .. vl .. ")")
+	  return vl
 end
