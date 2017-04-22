9e1ece8a12b8640379442322fbda43c6616bdf7c
Martin Ellis <malard@gmail.com>
2017-04-22 19:36:38 +0100

    added: generic sky q/hd ip control driver for control4

---
+++ Control4/sky_ip_pulse-eight/p8sky.lua
@@ -0,0 +1,27 @@
+function P8INT:SEND_KEY(code)
+    if (code == -1) then
+	   return
+    end
+    local ip = Properties["Device IP Address"] or ""
+    local boxType = Properties["Sky Box Type"] or ""
+    local port = 5900
+    if (boxType == "Sky+ HD") then
+	   port = 49160
+    end
+    local splice = 12
+    local skyClient = C4:CreateTCPClient()
+	   :OnConnect(function(client)
+		 client:ReadAtLeast(1)
+	   end)
+	   :OnRead(function (client, data)
+		  if (string.len(data) < 24) then
+			 client:Write(string.sub(data, 0, splice))
+			 client:ReadAtLeast(1)
+			 splice = 1
+		  else
+			 client:Write(string.char(4,1,0,0,0,0,math.floor(224 + (code/16)), code % 16))
+			 client:Write(string.char(4,0,0,0,0,0,math.floor(224 + (code/16)), code % 16))
+		  end
+	   end)
+	   :Connect(ip, port)
+end
