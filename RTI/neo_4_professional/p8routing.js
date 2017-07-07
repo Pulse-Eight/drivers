c74e641f4f2b0be7d4c4b47885c9633aba088cee
Martin Ellis <malard@gmail.com>
2017-07-07 08:20:53 +0100

    two way driver support for RTI

--- RTI/neo_4_professional/p8routing.js
+++ RTI/neo_4_professional/p8routing.js
@@ -1,15 +1,36 @@
-//Copyright Pulse-Eight Limtied 2016
+//Copyright Pulse-Eight Limtied 2017
 
 System.Print("P8Routing: Initializing...\r\n");
 
 //
 //  Startup Code
 //
-//ResetCounter();
+var TIMER_INTERVAL = 1000;
+var routingUpdateTimer = new Timer();
+var TOTAL_INPUTS = 4;
 
 //
 //  Internal Functions
 //
+function UpdateRouting() {
+	try {
+		var data = JSON.parse(System.GetURL(MATRIX_URL + '/Port/List'));
+		
+		for (var key in data.Ports) {
+			if (data.Ports[key].Mode == 'Output') {
+				for (var i = 0; i < TOTAL_INPUTS; i++) {
+					SystemVars.Write('O' + data.Ports[key].Bay + 'I' + i, data.Ports[key].ReceiveFrom == i);
+				}
+			}
+		}
+	} catch (err) {
+		System.Print(err);
+	    routingUpdateTimer.Start(UpdateRouting, TIMER_INTERVAL);
+	}
+}
+
+routingUpdateTimer.Start(UpdateRouting, TIMER_INTERVAL);
+
 
 //
 //  External Functions
