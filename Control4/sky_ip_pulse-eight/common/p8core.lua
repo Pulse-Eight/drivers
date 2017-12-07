ed700242985281121b2280f29fb3336c3df743ed
Martin Ellis <malard@gmail.com>
2017-12-07 17:36:05 +0000

    updated: sky q/hd ip driver for control 4 to support Sky Q Q060 firmware changes.

--- Control4/sky_ip_pulse-eight/common/p8core.lua
+++ Control4/sky_ip_pulse-eight/common/p8core.lua
@@ -1,23 +1,11 @@
---Copyright Pulse-Eight Limited 2015
+--Copyright Pulse-Eight Limited 2017
 
-function P8INT:GET_MATRIX_URL()
-    local ip = Properties["Device IP Address"] or ""
-    return "http://" .. ip
+function ON_PROPERTY_CHANGED.DeviceIPAddress(propertyValue)
+    P8INT:SETUP()
 end
 
-function P8INT:DISCOVER()
-    C4:urlGet("http://www.gotomymatrix.com", {}, false,
-	   function(ticketId, strData, responseCode, tHeaders, strError)
-		  if responseCode == 302 then
-			 local httpLocation = tHeaders["Location"]
-			 httpLocation = string.gsub(httpLocation, "http://", "")
-			 httpLocation = string.gsub(httpLocation, "/", "")
-			 UpdateProperty("Device IP Address", httpLocation)
-			 LogInfo("Device IP Address has been updated to: " .. httpLocation)
-		  else
-			 LogWarn("Failed to discover your system, please visit www.gotomymatrix.com in your browser for more information")
-		  end
-	   end)
+function ON_PROPERTY_CHANGED.SkyBoxType(propertyValue)
+    P8INT:SETUP()
 end
 
 function string.starts(String,Start)
--- Control4/sky_ip_pulse-eight/p8sky.lua
+++ Control4/sky_ip_pulse-eight/p8sky.lua
@@ -1,15 +1,40 @@
 require "lib.json"
 
+PORT = 5900
+
+function P8INT:SETUP()
+    LogInfo("Setup Running")
+    local boxType = Properties["Sky Box Type"] or ""
+    if (boxType == "Sky Q") then
+	local ip = Properties["Device IP Address"] or ""
+	local uri = "http://" .. ip .. ":9006/as/system/information"
+	C4:urlGet(uri, {}, false,
+	   function(ticketId, strData, responseCode, tHeaders, strError)
+		  if responseCode == 200 then
+			 local jsonResponse = JSON:decode(strData)
+			 UpdateProperty("Firmware Version", jsonResponse.modelNumber)
+			 local rev = string.match(jsonResponse.modelNumber, "([^.]+)")
+			 local version = tonumber(string.sub(rev, 2))
+			 if (version > 50) then
+				LogInfo("Sky Q Version > 50, setting to new port 49160")
+				PORT = 49160
+			 else
+				LogInfo("Sky Q Version <= 50, setting to old port 5900")
+				PORT = 5900
+			 end
+		  end
+	   end)
+    else
+	   LogInfo("Setting port to 49160 as not Sky Q box")
+	   PORT = 49160
+    end
+end
+
 function P8INT:SEND_KEY(code)
     if (code == -1) then
 	   return
     end
     local ip = Properties["Device IP Address"] or ""
-    local boxType = Properties["Sky Box Type"] or ""
-    local port = 5900
-    if (boxType == "Sky+ HD") then
-	   port = 49160
-    end
     local splice = 12
     local skyClient = C4:CreateTCPClient()
 	   :OnConnect(function(client)
@@ -25,13 +50,13 @@ function P8INT:SEND_KEY(code)
 			 client:Write(string.char(4,0,0,0,0,0,math.floor(224 + (code/16)), code % 16))
 		  end
 	   end)
-	   :Connect(ip, port)
+	   :Connect(ip, PORT)
 end
 
 function P8INT:TURN_ON()
     local boxType = Properties["Sky Box Type"] or ""
     if (boxType == "Sky+ HD") then
-	   P8INT:SEND_KEY(11)
+	   P8INT:SEND_KEY(0)
     end
     
     local ip = Properties["Device IP Address"] or ""
@@ -41,7 +66,7 @@ function P8INT:TURN_ON()
 		  if responseCode == 200 then
 			 local jsonResponse = JSON:decode(strData)
 			 if (jsonResponse.activeStandby) then
-				P8INT:SEND_KEY(11)
+				P8INT:SEND_KEY(0)
 			 end
 		  end
 	   end)
