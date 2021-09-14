b9f2eafd477905e8ae0cb3f693cdcaf596fc23d9
Martin Ellis <malard@gmail.com>
2021-09-14 10:28:46 +0100

    added: XMR and XSR drivers and updated documentation

---
+++ Control4/neoXMR_pulse-eight_binary/common/p8init.lua
@@ -0,0 +1,113 @@
+require "common.p8declares"
+require "common.p8properties"
+
+require "lib.c4_log"
+require "lib.c4_timer"
+
+gForceLogging = false
+
+function ON_DRIVER_INIT.p8init()
+	-- Create Log Timer
+	gC4LogTimer = c4_timer:new("Log Timer", 45, "MINUTES", OnLogTimerExpired)
+end
+
+function OnLogTimerExpired()
+	LogWarn("Turning Log Mode Off (timer expired)")
+	gC4LogTimer:KillTimer()
+	
+	C4:UpdateProperty("Log Mode", "Off")
+	OnPropertyChanged("Log Mode")
+end
+
+function ON_PROPERTY_CHANGED.LogMode(propertyValue)
+	gC4LogTimer:KillTimer()
+	
+	if (gForceLogging) then
+		LOG:OutputPrint(true)
+		LOG:OutputC4Log(true)
+	else
+		LOG:OutputPrint(propertyValue:find("Print") ~= nil)
+		LOG:OutputC4Log(propertyValue:find("Log") ~= nil)
+		if (propertyValue == "Off") then
+			return
+		end
+		
+		gC4LogTimer:StartTimer()
+	end
+end
+
+function ON_PROPERTY_CHANGED.LogLevel(propertyValue)
+	if (gForceLogging) then
+		LOG:SetLogLevel("5 - Debug")
+	else
+		LOG:SetLogLevel(propertyValue)
+	end
+end
+
+
+-- Main calls from the Director
+
+function OnDriverInit()
+    gInitializingDriver = true
+    C4:ErrorLog("INIT_CODE: OnDriverInit()")
+
+	-- Call all ON_DRIVER_EARLY_INIT functions.
+	for k,v in pairs(ON_DRIVER_EARLY_INIT) do
+		if (ON_DRIVER_EARLY_INIT[k] ~= nil and type(ON_DRIVER_EARLY_INIT[k]) == "function") then
+			C4:ErrorLog("INIT_CODE: ON_DRIVER_EARLY_INIT." .. k .. "()")
+			print("INIT_CODE: ON_DRIVER_EARLY_INIT." .. k .. "()")
+			local status, err = pcall(ON_DRIVER_EARLY_INIT[k])
+			if (not status) then
+				LogError("LUA_ERROR: " .. err)
+			end
+		end
+	end
+
+	-- Call all ON_DRIVER_INIT functions
+	for k,v in pairs(ON_DRIVER_INIT) do
+		if (ON_DRIVER_INIT[k] ~= nil and type(ON_DRIVER_INIT[k]) == "function") then
+			C4:ErrorLog("INIT_CODE: ON_DRIVER_INIT." .. k .. "()")
+			print("INIT_CODE: ON_DRIVER_INIT." .. k .. "()")
+			local status, err = pcall(ON_DRIVER_INIT[k])
+			if (not status) then
+				LogError("LUA_ERROR: " .. err)
+			end
+		end
+	end
+
+	-- Fire OnPropertyChanged to set the initial Headers and other Property
+	-- global sets, they'll change if Property is changed.
+	for k,v in pairs(Properties) do
+		C4:ErrorLog("INIT_CODE: Calling OnPropertyChanged - " .. k .. ": " .. v)
+		local status, err = pcall(OnPropertyChanged, k)
+		if (not status) then
+			LogError("LUA_ERROR: " .. err)
+		end
+	end
+
+	gInitializingDriver = false
+end
+
+function OnDriverLateInit()
+	C4:ErrorLog("INIT_CODE: OnDriverLateInit()")
+	
+	-- Call all ON_DRIVER_LATEINIT functions
+	for k,v in pairs(ON_DRIVER_LATEINIT) do
+		if (ON_DRIVER_LATEINIT[k] ~= nil and type(ON_DRIVER_LATEINIT[k]) == "function") then
+			C4:ErrorLog("INIT_CODE: ON_DRIVER_LATEINIT." .. k .. "()")
+			ON_DRIVER_LATEINIT[k]()
+		end
+	end
+end
+
+function OnDriverDestroyed()
+	C4:ErrorLog("INIT_CODE: OnDriverDestroyed()")
+	
+	-- Call all ON_DRIVER_DESTROYED functions
+	for k, v in pairs(ON_DRIVER_DESTROYED) do
+		if (ON_DRIVER_DESTROYED[k] ~= nil and type(ON_DRIVER_DESTROYED[k]) == "function") then
+			C4:ErrorLog("INIT_CODE: ON_DRIVER_DESTROYED." .. k .. "()")
+			ON_DRIVER_DESTROYED[k]()
+		end
+	end
+end
---
+++ Control4/neoXSR_pulse-eight_binary/p8driver.lua
@@ -0,0 +1,80 @@
+require "common.p8declares"
+require "common.p8init"
+require "common.p8core"
+require "common.c4_command"
+require "common.c4_notify"
+require "common.c4_diagnostics" --DEBUG
+require "p8proxy"
+require "p8system"
+require "p8routing"
+require "p8monitoring"
+
+--Defines
+
+MAX_INPUTS = 12
+MAX_OUTPUTS = 10
+
+--Init Functions
+
+local gP8RoutingUpdateTimer
+local gP8DetailsTimer
+local gP8HealthTimer
+local gP8VolumeTimer
+
+function ON_DRIVER_EARLY_INIT.main()
+    C4:AddVariable("Temp0", 0, "NUMBER")
+end
+
+function ON_DRIVER_INIT.main()
+    FirstRun()
+end
+
+function FirstRun()
+    --Init Connected Devices
+    LogInfo("First Run")
+    P8INT:SETUP()
+    P8INT:FETCH_INSTALLER_ID()
+    gP8RoutingUpdateTimer = c4_timer:new("Routing Update", 2, "SECONDS", RoutingUpdateTimer, true)
+    gP8RoutingUpdateTimer:StartTimer()
+    gP8DetailsTimer = c4_timer:new("Details Update", 10, "SECONDS", DetailsUpdateTimer, true)
+    gP8DetailsTimer:StartTimer()
+    gP8HealthTimer = c4_timer:new("Health Update", 1, "MINUTES", HealthUpdateTimer, true)
+    gP8HealthTimer:StartTimer()
+    gP8VolumeTimer = c4_timer:new("Volume Update", 10, "SECONDS", VolumeUpdateTimer, true)
+    gP8VolumeTimer:StartTimer()
+    --Call once to set initial states
+    RoutingUpdateTimer()
+    DetailsUpdateTimer()
+    HealthUpdateTimer()
+    VolumeUpdateTimer()
+    --Register for SDDP Setup
+    local deviceId = C4:GetDeviceID()
+    C4:RegisterSystemEvent(C4SystemEvents["OnSDDPDeviceStatus"], deviceId)
+    C4:RegisterSystemEvent(C4SystemEvents["OnDiscoveredDeviceChanged"], deviceId)
+    C4:RegisterSystemEvent(C4SystemEvents["OnNetworkBindingAdded"], deviceId) --This is triggered when the user maps the IP in the Network Connections Tab
+    C4:RegisterSystemEvent(C4SystemEvents["OnDeviceIPAddressChanged"], deviceId)
+    C4:RegisterSystemEvent(C4SystemEvents["OnDiscoveredDeviceAdded"], deviceId)
+    C4:RegisterSystemEvent(C4SystemEvents["OnSDDPDeviceDiscover"], deviceId)
+    C4:RegisterSystemEvent(C4SystemEvents["OnDeviceDiscovered"], deviceId)
+end
+
+function VolumeUpdateTimer()
+    for i = 1,MAX_OUTPUTS do
+	   --P8INT:UPDATE_AUDIO(DEFAULT_PROXY_BINDINGID, 4000 + i - 1)
+    end
+end
+
+function RoutingUpdateTimer()
+    if (Properties["Auto Sync Navigators"] == "Yes") then
+	   P8INT:GET_ROUTING(DEFAULT_PROXY_BINDINGID)
+	   P8INT:GET_POWER_STATE(DEFAULT_PROXY_BINDINGID)
+    end
+end
+
+function DetailsUpdateTimer()
+    P8INT:GET_DETAILS(DEFAULT_PROXY_BINDINGID)
+end
+
+function HealthUpdateTimer()
+    P8INT:GET_HEALTH(DEFAULT_PROXY_BINDINGID)
+end
