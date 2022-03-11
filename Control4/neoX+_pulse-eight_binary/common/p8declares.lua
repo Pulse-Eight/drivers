419c16ff046a2140acfcfe38af9262309d4ac039
Ben Potter <ben@bpuk.org>
2022-03-11 09:32:24 +0000

    Updated: Source mode changes for binary NeoX+ driver

--- Control4/neoX+_pulse-eight_binary/common/p8declares.lua
+++ Control4/neoX+_pulse-eight_binary/common/p8declares.lua
@@ -19,6 +19,13 @@ ON_PROPERTY_CHANGED = {}
 -- Constants
 DEFAULT_PROXY_BINDINGID = 5001
 
+-- Driver wide properties
+-- Sink mode defaults to 'Sink mode'. Source mode on 0. Store as a variable to reduce the number of string comparisions required
+MODE_SINK = 1 
+MODE_POWERON_ON_ROUTING_CHANGE = 1
+MODE_POWEROFF_ON_ZONE_OFF = 0
+
+
 
 -- I don't know why this has to be here, if its placed in p8init.lua it fails and the script does not work. If anyone knows why please let me know
 
